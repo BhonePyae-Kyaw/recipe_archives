81cd153652e9f8f081c5c2b53abed2b8354e2c11
Bhone Pyae Kyaw <137211983+BhonePyae-Kyaw@users.noreply.github.com>
2024-09-23 18:59:12 +0700

    Feat: combine codes and next instructions

---
+++ app/recipe/[id]/page.js
@@ -0,0 +1,8 @@
+export default async function Home() {
+  return (
+    //get recipe by ID
+    <div className="m-4">
+      <h1>Recipe Page by ID\</h1>
+    </div>
+  );
+}
---
+++ app/review/[id]/page.js
@@ -0,0 +1,7 @@
+export default async function Home() {
+  return (
+    <div className="m-4">
+      <h1>Review Page by ID</h1>
+    </div>
+  );
+}
